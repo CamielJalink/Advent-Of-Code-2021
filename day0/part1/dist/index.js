2166c235194b7bb4f94776931c77936a65ec4ea1
Camiel Jalink <camiel.jalink@gmail.com>
2021-11-28 22:38:42 +0100

    Removed eslint and updated tsconfig

--- day0/part1/dist/index.js
+++ day0/part1/dist/index.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = require("fs");
 function advent() {
-    var stringInput = fs_1.readFileSync("input.txt", "utf-8");
-    var input = stringInput.split("\n");
+    var stringInput = (0, fs_1.readFileSync)("input.txt", "utf-8");
+    var input = stringInput.split("\r\n");
     console.log(input);
 }
 advent();
--- day0/part1/src/index.ts
+++ day0/part1/src/index.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs";
 
 function advent() {
-  const stringInput: string = readFileSync("input.txt", "utf-8");
+  const stringInput = readFileSync("input.txt", "utf-8");
   const input: string[] = stringInput.split("\r\n");
   console.log(input);
 }

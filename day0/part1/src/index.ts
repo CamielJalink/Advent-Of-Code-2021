95d607a5a119c0e866d4cbeabb86c97d62a1a67f
Camiel Jalink <camiel.jalink@gmail.com>
2021-11-28 22:39:53 +0100

    removed explicit typing on input

--- day0/part1/src/index.ts
+++ day0/part1/src/index.ts
@@ -2,7 +2,7 @@ import { readFileSync } from "fs";
 
 function advent() {
   const stringInput = readFileSync("input.txt", "utf-8");
-  const input: string[] = stringInput.split("\r\n");
+  const input = stringInput.split("\r\n");
   console.log(input);
 }
 

b83c1fa7b72d0e2bfe02a714ca07f2aa051a752c
Camiel Jalink <camiel.jalink@gmail.com>
2021-11-28 21:44:45 +0100

    attempt 2 at initial commit

---
+++ day0/part1/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = require("fs");
+function advent() {
+    var stringInput = fs_1.readFileSync("input.txt", "utf-8");
+    var input = stringInput.split("\n");
+    console.log(input);
+}
+advent();
---
+++ day0/part1/src/index.ts
@@ -0,0 +1,9 @@
+import { readFileSync } from "fs";
+
+function advent() {
+  const stringInput: string = readFileSync("input.txt", "utf-8");
+  const input: string[] = stringInput.split("\r\n");
+  console.log(input);
+}
+
+advent();

0f92dba57f6c1ff17ac8317b4ab7f275dfdbcfed
Camiel <camiel.jalink@gmail.com>
2021-11-29 22:13:57 +0100

    Added eslint setup without a ruleset (yet)

--- day0/part1/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = require("fs");
-function advent() {
-    var stringInput = (0, fs_1.readFileSync)("input.txt", "utf-8");
-    var input = stringInput.split("\r\n");
-    console.log(input);
-}
-advent();

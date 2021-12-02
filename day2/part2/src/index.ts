889202b963a0b16ec9fdee1e41945d899c42257a
Camiel <camiel.jalink@gmail.com>
2021-12-02 07:29:17 +0100

    Copied part2 from part1

---
+++ day2/part2/src/index.ts
@@ -0,0 +1,39 @@
+import { readFileSync } from "fs";
+
+function advent() {
+    const stringInput = readFileSync("input.txt", "utf-8");
+    const inputArray = stringInput.split("\r\n");
+    const movesArray: string[][] = [];
+
+    inputArray.forEach((stringMove) => {
+        movesArray.push(stringMove.split(" "));
+    });
+
+    moveSub(movesArray);
+}
+
+function moveSub(movesArray: string[][]) {
+    let xPos = 0;
+    let depth = 0;
+
+    movesArray.forEach((move) => {
+        switch (move[0]) {
+            case "forward":
+                xPos += Number(move[1]);
+                break;
+            case "down":
+                depth += Number(move[1]);
+                break;
+            case "up":
+                depth -= Number(move[1]);
+                break;
+            default:
+                console.error("Something is wrong with my input!");
+                break;
+        }
+    });
+
+    console.log(`Sub is at xPos ${xPos}, depth ${depth} which results in score ${xPos * depth}`);
+}
+
+advent();

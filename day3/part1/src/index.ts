9a716735741809e88f3ace90e7e97f834ab1b241
Camiel <camiel.jalink@gmail.com>
2021-12-02 07:44:36 +0100

    copied day3 from day0 template

---
+++ day3/part1/src/index.ts
@@ -0,0 +1,9 @@
+import { readFileSync } from "fs";
+
+function advent() {
+    const stringInput = readFileSync("input.txt", "utf-8");
+    const input = stringInput.split("\r\n");
+    console.log(input);
+}
+
+advent();

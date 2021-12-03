12fc388b68b39f850568c6e768bc2e6bbcd09704
Camiel <camiel.jalink@gmail.com>
2021-12-03 16:09:39 +0100

    Day3part2 solution

--- day3/part2/src/index.ts
+++ day3/part2/src/index.ts
@@ -3,37 +3,75 @@ import { readFileSync } from "fs";
 function advent() {
     const stringInput = readFileSync("input.txt", "utf-8");
     const input = stringInput.split("\r\n");
-    pwrCons(input);
+    lifeSupport(input);
 }
 
-function pwrCons(input: string[]) {
+function lifeSupport(input: string[]) {
+    // Count the ammount of ones at each position.
     const sums: number[] = [];
     for (let i = 0; i < input[0].length; i++) {
         sums[i] = 0;
     }
 
+    // determine the average for each position (between 0 and 1)
     input.forEach((binary) => {
         for (let i = 0; i < binary.length; i++) {
             sums[i] += parseInt(binary[i]);
         }
     });
 
-    const averages = sums.map((sum) => {
-        return sum / input.length;
+    // determine the most found digit, 1 or 0, at each position
+    const rounded: number[] = [];
+    sums.forEach((sum) => {
+        rounded.push(Math.round(sum / input.length));
     });
 
-    let gammaString = "";
-    let epsilonString = "";
-    averages.forEach((average) => {
-        gammaString += String(Math.round(average));
-        epsilonString += String(Math.round(1 - average));
-    });
+    const oxygenBinary = findBinary(input, rounded, 1);
+    const scrubberBinary = findBinary(input, rounded, 0);
+    console.log(`life support rating is ${oxygenBinary} * ${scrubberBinary} = ${oxygenBinary * scrubberBinary}`);
+}
 
-    const gamma = parseInt(gammaString, 2);
-    const epsilon = parseInt(epsilonString, 2);
-    console.log(`Gamma is: ${gamma}`);
-    console.log(`Epsilon is: ${epsilon}`);
-    console.log(`Power consumption is ${gamma * epsilon}`);
+function findBinary(input: string[], rounded: number[], tieBreaker: number) {
+    let inputCopy: string[] = JSON.parse(JSON.stringify(input));
+    let targetBinary = "";
+
+    for (let i = 0; i < rounded.length; i++) {
+        const zeroInputs: string[] = [];
+        const oneInputs: string[] = [];
+
+        // Determine the amount of binaries with a 1 or a 0 at this position.
+        for (let j = 0; j < inputCopy.length; j++) {
+            if (inputCopy[j][i] === String(1)) {
+                oneInputs.push(inputCopy[j]);
+            } else {
+                zeroInputs.push(inputCopy[j]);
+            }
+        }
+
+        // Depending on whether we are tracking oxygen (1) or CO2 scrubbing (0), use a different bit criterium.
+        if (tieBreaker === 1) {
+            if (oneInputs.length >= zeroInputs.length) {
+                inputCopy = oneInputs;
+            } else {
+                inputCopy = zeroInputs;
+            }
+        }
+
+        if (tieBreaker === 0) {
+            if (zeroInputs.length <= oneInputs.length) {
+                inputCopy = zeroInputs;
+            } else {
+                inputCopy = oneInputs;
+            }
+        }
+
+        if (inputCopy.length === 1) {
+            targetBinary = inputCopy[0];
+            break;
+        }
+    }
+
+    return parseInt(targetBinary, 2);
 }
 
 advent();

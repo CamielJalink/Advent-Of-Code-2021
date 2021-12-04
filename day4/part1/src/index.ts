ef68e83813131d1a959d896adefa6d65a77123f0
Camiel <camiel.jalink@gmail.com>
2021-12-04 12:23:51 +0100

    Day4Part1 solution

--- day4/part1/src/index.ts
+++ day4/part1/src/index.ts
@@ -1,9 +1,35 @@
 import { readFileSync } from "fs";
+import BingoBoard from "./bingoBoard";
 
 function advent() {
     const stringInput = readFileSync("input.txt", "utf-8");
-    const input = stringInput.split("\r\n");
-    console.log(input);
+    const input = stringInput.split("\r\n\r\n");
+    playBingo(input);
+}
+
+function playBingo(input: string[]) {
+    const chosenNumbers = (input.shift() || "").split(",").map((stringNumber) => parseInt(stringNumber));
+
+    const bingoBoards: BingoBoard[] = [];
+    input.forEach((stringBoard: string) => {
+        bingoBoards.push(new BingoBoard(stringBoard, chosenNumbers));
+    });
+
+    bingoBoards.forEach((bingoBoard) => {
+        bingoBoard.playBingo();
+    });
+
+    // Check which bingoBoard finished first. That boards score is the winning score!
+    let fastestWin = chosenNumbers.length;
+    let winningScore;
+    bingoBoards.forEach((bingoBoard) => {
+        if (bingoBoard.winningTurn < fastestWin) {
+            fastestWin = bingoBoard.winningTurn;
+            winningScore = bingoBoard.score;
+        }
+    });
+
+    console.log(`The highest score found is: ${winningScore}`);
 }
 
 advent();

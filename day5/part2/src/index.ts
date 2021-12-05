85287c80fe98236b82d5da382aaa398f954e195d
Camiel <camiel.jalink@gmail.com>
2021-12-05 13:01:16 +0100

    Day5Part2 solution

--- day5/part2/src/index.ts
+++ day5/part2/src/index.ts
@@ -30,12 +30,14 @@ function parseCoordinates(input: string[]): number[][] {
         const lineCoordsStr = line.split(" -> ");
         const start = lineCoordsStr[0].split(",").map((char) => parseInt(char));
         const end = lineCoordsStr[1].split(",").map((char) => parseInt(char));
+        let startY = start[1];
+        let endY = end[1];
+        let startX = start[0];
+        let endX = end[0];
 
         // If x coord is the same, we are looking at a vertical line!
         if (start[0] === end[0]) {
-            // Lines can go forward or backward!
-            let startY = start[1];
-            let endY = end[1];
+            // Lines can go forward or backward, so flip the 'starting' point.
             if (start[1] >= end[1]) {
                 startY = end[1];
                 endY = start[1];
@@ -47,16 +49,36 @@ function parseCoordinates(input: string[]): number[][] {
         }
         // If y coords are the same, it's a horizontal line!
         else if (start[1] === end[1]) {
-            // Lines can go forward or backward!
-            let startX = start[0];
-            let endX = end[0];
             if (start[0] >= end[0]) {
                 startX = end[0];
                 endX = start[0];
             }
-            // determine coords in the line and add them to the array
             for (let x = startX; x <= endX; x++) {
                 coords.push([x, start[1]]);
+            }
+        }
+        // else, we have a vertical line!
+        else {
+            // determine our starting point based on x coordinate.
+            let diagStart = start;
+            let diagEnd = end;
+            if (start[0] >= end[0]) {
+                diagStart = end;
+                diagEnd = start;
+            }
+
+            // Since all our angles are 45 degrees, the xDiff and yDiff are always the same. Only need one.
+            const diff = diagEnd[0] - diagStart[0];
+
+            // determine the coordinates based on whether the diagonal line is rising or falling.
+            if (diagStart[1] < diagEnd[1]) {
+                for (let i = 0; i <= diff; i++) {
+                    coords.push([diagStart[0] + i, diagStart[1] + i]);
+                }
+            } else {
+                for (let i = 0; i <= diff; i++) {
+                    coords.push([diagStart[0] + i, diagStart[1] - i]);
+                }
             }
         }
     });

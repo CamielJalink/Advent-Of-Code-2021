042c61180cf6b36b42253e652cec427587fadcd4
Camiel <camiel.jalink@gmail.com>
2021-12-06 07:41:15 +0100

    day6part1 solution

--- day6/part1/src/index.ts
+++ day6/part1/src/index.ts
@@ -3,7 +3,27 @@ import { readFileSync } from "fs";
 function advent() {
     const stringInput = readFileSync("input.txt", "utf-8");
     const input = stringInput.split("\r\n");
-    console.log(input);
+    const fish = input[0].split(",").map((stringFish) => parseInt(stringFish));
+    console.log(simulateFish(fish));
+}
+
+function simulateFish(allFish: number[]) {
+    for (let day = 1; day <= 80; day++) {
+        const prevFish: number[] = [];
+        const newFish: number[] = [];
+
+        allFish.forEach((fish) => {
+            if (fish === 0) {
+                fish = 6;
+                newFish.push(8);
+            } else {
+                fish--;
+            }
+            prevFish.push(fish);
+        });
+        allFish = [...prevFish, ...newFish];
+    }
+    return allFish.length;
 }
 
 advent();

182150eca35fb6d76e2b83a28689fe645c25e6fd
Camiel <camiel.jalink@gmail.com>
2021-12-08 07:42:10 +0100

    Day8Part1 solution

--- day8/part1/src/index.ts
+++ day8/part1/src/index.ts
@@ -3,7 +3,29 @@ import { readFileSync } from "fs";
 function advent() {
     const stringInput = readFileSync("input.txt", "utf-8");
     const input = stringInput.split("\r\n");
-    console.log(input);
+    console.log(countOutputDigits(input));
+}
+
+function countOutputDigits(input: string[]) {
+    const outputs: string[] = [];
+
+    for (let i = 0; i < input.length; i++) {
+        const output = input[i].split(" | ")[1];
+        outputs.push(output);
+    }
+
+    let outputWords: string[] = [];
+    outputs.forEach((output: string) => {
+        outputWords = [...outputWords, ...output.split(" ")];
+    });
+
+    let numUniqueDigits = 0;
+    outputWords.forEach((word) => {
+        if (word.length === 2 || word.length === 3 || word.length === 4 || word.length === 7) {
+            numUniqueDigits++;
+        }
+    });
+    return numUniqueDigits;
 }
 
 advent();

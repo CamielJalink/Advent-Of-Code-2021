a6b706952e70057ea1a681ec9e1c0fd1b7590365
Camiel <camiel.jalink@gmail.com>
2021-12-10 10:37:15 +0100

    Day10part1 solution

--- day10/part1/src/index.ts
+++ day10/part1/src/index.ts
@@ -3,7 +3,69 @@ import { readFileSync } from "fs";
 function advent() {
     const stringInput = readFileSync("input.txt", "utf-8");
     const input = stringInput.split("\r\n");
-    console.log(input);
+    console.log(findSyntaxErrorScore(input));
 }
 
 advent();
+
+function findSyntaxErrorScore(input: string[]) {
+    let syntaxErrorScore = 0;
+    input.forEach((line: string) => {
+        syntaxErrorScore += parseLine(line);
+    });
+    return syntaxErrorScore;
+}
+
+function parseLine(line: string) {
+    const openingBrackets: string[] = [];
+    let wrongClosingBracket = "error";
+
+    for (const c of line) {
+        if (c === "(" || c === "[" || c === "{" || c === "<") {
+            openingBrackets.push(c);
+        } else {
+            const prev = openingBrackets[openingBrackets.length - 1];
+            if (c === ")") {
+                if (prev === "(") {
+                    openingBrackets.pop();
+                } else {
+                    wrongClosingBracket = c;
+                    break;
+                }
+            } else if (c === "]") {
+                if (prev === "[") {
+                    openingBrackets.pop();
+                } else {
+                    wrongClosingBracket = c;
+                    break;
+                }
+            } else if (c === "}") {
+                if (prev === "{") {
+                    openingBrackets.pop();
+                } else {
+                    wrongClosingBracket = c;
+                    break;
+                }
+            } else if (c === ">") {
+                if (prev === "<") {
+                    openingBrackets.pop();
+                } else {
+                    wrongClosingBracket = c;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (wrongClosingBracket === ")") {
+        return 3;
+    } else if (wrongClosingBracket === "]") {
+        return 57;
+    } else if (wrongClosingBracket === "}") {
+        return 1197;
+    } else if (wrongClosingBracket === ">") {
+        return 25137;
+    } else {
+        return 0;
+    }
+}

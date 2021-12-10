198254b12daa1fcf4b07a7964398484af283c83a
Camiel <camiel.jalink@gmail.com>
2021-12-10 11:29:28 +0100

    Day10part2 solution

--- day10/part2/src/index.ts
+++ day10/part2/src/index.ts
@@ -3,17 +3,70 @@ import { readFileSync } from "fs";
 function advent() {
     const stringInput = readFileSync("input.txt", "utf-8");
     const input = stringInput.split("\r\n");
-    console.log(findSyntaxErrorScore(input));
+    console.log(findCompletionStringScore(input));
 }
 
 advent();
 
-function findSyntaxErrorScore(input: string[]) {
-    let syntaxErrorScore = 0;
+function findCompletionStringScore(input: string[]) {
+    const remainingOpeningLines: string[][] = [];
     input.forEach((line: string) => {
-        syntaxErrorScore += parseLine(line);
+        const remainingOpenings: string[] = parseLine(line);
+        if (remainingOpenings.length > 0) {
+            remainingOpeningLines.push(remainingOpenings);
+        }
     });
-    return syntaxErrorScore;
+    const completionStringScores: number[] = [];
+    remainingOpeningLines.forEach((openings: string[]) => {
+        completionStringScores.push(determineCompletionScore(openings));
+    });
+    completionStringScores.sort((a, b) => a - b);
+    return completionStringScores[Math.ceil(completionStringScores.length / 2) - 1];
+}
+
+// the openings contain all the remaining openings of a line that was incomplete.
+function determineCompletionScore(openings: string[]) {
+    const closingBrackets: string[] = [];
+    openings.forEach((opening: string) => {
+        switch (opening) {
+            case "(":
+                closingBrackets.unshift(")");
+                break;
+            case "[":
+                closingBrackets.unshift("]");
+                break;
+            case "{":
+                closingBrackets.unshift("}");
+                break;
+            case "<":
+                closingBrackets.unshift(">");
+                break;
+            default:
+                console.error("parsing closing brackets broke!");
+        }
+    });
+
+    let completionScore = 0;
+    closingBrackets.forEach((closing: string) => {
+        completionScore *= 5;
+        switch (closing) {
+            case ")":
+                completionScore += 1;
+                break;
+            case "]":
+                completionScore += 2;
+                break;
+            case "}":
+                completionScore += 3;
+                break;
+            case ">":
+                completionScore += 4;
+                break;
+            default:
+                console.error("calculating completion score broke!");
+        }
+    });
+    return completionScore;
 }
 
 function parseLine(line: string) {
@@ -57,15 +110,11 @@ function parseLine(line: string) {
         }
     }
 
-    if (wrongClosingBracket === ")") {
-        return 3;
-    } else if (wrongClosingBracket === "]") {
-        return 57;
-    } else if (wrongClosingBracket === "}") {
-        return 1197;
-    } else if (wrongClosingBracket === ">") {
-        return 25137;
+    // If this is the case, then we have an incomplete line.
+    if (wrongClosingBracket === "error") {
+        // return the remaining openingbrackets!
+        return openingBrackets;
     } else {
-        return 0;
+        return [];
     }
 }

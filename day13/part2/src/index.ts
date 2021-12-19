55034d022c845f4d1028542bec44242d2ff25e2d
Camiel <camiel.jalink@gmail.com>
2021-12-19 20:29:18 +0100

    Day13Part2 solution

--- day13/part2/src/index.ts
+++ day13/part2/src/index.ts
@@ -3,27 +3,24 @@ import { readFileSync } from "fs";
 function advent() {
     const stringInput = readFileSync("input.txt", "utf-8");
     const input = stringInput.split("\r\n\r\n");
-    console.log(countDots(input));
+    foldInstructions(input);
 }
 
-function countDots(input: string[]) {
+function foldInstructions(input: string[]) {
     const map: string[][] = buildMap(input);
 
-    let numberOfDots = 0;
     map.forEach((row: string[]) => {
+        let drawRow = "";
         row.forEach((coord: string) => {
-            if (coord === "#") {
-                numberOfDots++;
-            }
+            drawRow += coord;
         });
+        console.log(drawRow);
     });
-
-    return numberOfDots;
 }
 
 function buildMap(input: string[]) {
     const coordsStrings: string[] = input[0].split("\r\n");
-    const coords: number[][] = [];
+    let coords: number[][] = [];
     let maxX = 0;
     let maxY = 0;
 
@@ -51,22 +48,56 @@ function buildMap(input: string[]) {
 
     // For all the coordinates in the input, draw a #
     map = drawDots(map, coords);
+    const instructionLines: string[] = input[1].split("\r\n");
+    const drawInstructions: string[][] = instructionLines.map((instructionLine: string) => {
+        return instructionLine.split(" ")[2].split("=");
+    });
 
-    const drawInstruction: string[] = input[1].split("\r\n")[0].split(" ")[2].split("=");
-
-    if (drawInstruction[0] === "y") {
-        console.log("now folding up");
-        map = foldUp(map, coords, parseInt(drawInstruction[1])); // Hardcoded y-axis foldline, will fix this later.
-    } else if (drawInstruction[0] === "x") {
-        console.log("now folding left");
-        map = foldLeft(map, coords, parseInt(drawInstruction[1]));
-    }
+    drawInstructions.forEach((drawInstruction: string[]) => {
+        if (drawInstruction[0] === "y") {
+            map = foldUp(map, parseInt(drawInstruction[1]));
+            coords = fixCoordsFoldUp(coords, parseInt(drawInstruction[1]));
+        } else if (drawInstruction[0] === "x") {
+            map = foldLeft(map, parseInt(drawInstruction[1]));
+            coords = fixCoordsLeftFold(coords, parseInt(drawInstruction[1]));
+        }
+        map = drawDots(map, coords);
+    });
 
     return map;
 }
 
-function foldLeft(map: string[][], coords: number[][], foldLine: number) {
-    let foldedMap: string[][] = [];
+function foldUp(map: string[][], foldLine: number) {
+    const foldedMap: string[][] = [];
+    // Throw away the lines we no longer need: the part we folded away.
+    for (let i = 0; i < foldLine; i++) {
+        foldedMap.push(map[i]);
+    }
+
+    return foldedMap;
+}
+
+function fixCoordsFoldUp(coords: number[][], foldLine: number) {
+    // Determine which coords were on the folded-away part, and determine their new y-coordinate.
+    const newCoords: number[][] = [];
+    coords.forEach((coord: number[]) => {
+        if (coord[1] > foldLine) {
+            const foldedCoord: number[] = [];
+            foldedCoord[0] = coord[0]; // the coord keeps the same x-coordinate
+            const lineAfterFold = coord[1] - foldLine;
+            foldedCoord[1] = foldLine - lineAfterFold;
+            newCoords.push(foldedCoord);
+        } else {
+            newCoords.push(coord);
+        }
+    });
+
+    return newCoords;
+}
+
+// Fold the map left, halving it in size.
+function foldLeft(map: string[][], foldLine: number) {
+    const foldedMap: string[][] = [];
 
     // Throw away the columns we no longer need: the part we folded away.
     for (let y = 0; y < map.length; y++) {
@@ -77,45 +108,26 @@ function foldLeft(map: string[][], coords: number[][], foldLine: number) {
         foldedMap.push(newRow);
     }
 
+    return foldedMap;
+}
+
+// determine the new set of coords we have
+function fixCoordsLeftFold(coords: number[][], foldLine: number) {
     // Determine which coords were on the folded-away part, and determine their new x-coordinate.
-    const foldedCoords: number[][] = [];
+    const newCoords: number[][] = [];
     coords.forEach((coord: number[]) => {
         if (coord[0] > foldLine) {
             const foldedCoord: number[] = [];
             foldedCoord[1] = coord[1]; // the coord keeps the same y-coordinate
             const lineAfterFold = coord[0] - foldLine;
             foldedCoord[0] = foldLine - lineAfterFold;
-            foldedCoords.push(foldedCoord);
+            newCoords.push(foldedCoord);
+        } else {
+            newCoords.push(coord);
         }
     });
 
-    // draw the folded coords on the smaller foldedmap.
-    foldedMap = drawDots(foldedMap, foldedCoords);
-    return foldedMap;
-}
-
-function foldUp(map: string[][], coords: number[][], foldLine: number) {
-    let foldedMap: string[][] = [];
-    // Throw away the lines we no longer need: the part we folded away.
-    for (let i = 0; i < foldLine; i++) {
-        foldedMap.push(map[i]);
-    }
-
-    // Determine which coords were on the folded-away part, and determine their new y-coordinate.
-    const foldedCoords: number[][] = [];
-    coords.forEach((coord: number[]) => {
-        if (coord[1] > foldLine) {
-            const foldedCoord: number[] = [];
-            foldedCoord[0] = coord[0]; // the coord keeps the same x-coordinate
-            const lineAfterFold = coord[1] - foldLine;
-            foldedCoord[1] = foldLine - lineAfterFold;
-            foldedCoords.push(foldedCoord);
-        }
-    });
-
-    // draw the folded coords on the smaller foldedmap.
-    foldedMap = drawDots(foldedMap, foldedCoords);
-    return foldedMap;
+    return newCoords;
 }
 
 function drawDots(map: string[][], coords: number[][]) {

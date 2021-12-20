94454f83fecca162f4b27863a151186138b1c49f
Camiel <camiel.jalink@gmail.com>
2021-12-20 18:06:21 +0100

    Day14Part2 solution

--- day14/part2/src/index.ts
+++ day14/part2/src/index.ts
@@ -7,42 +7,48 @@ function advent() {
 }
 
 function buildPolymer(input: string[]) {
+    const firstLetter = input[0][0];
+    const lastLetter = input[0][input[0].length - 1];
     let build = fillInitialMap(input[0]);
     const rules: string[][] = parseRules(input[1]);
 
-    for (let day = 0; day < 10; day++) {
+    for (let day = 0; day < 40; day++) {
         build = applyRules(build, rules);
     }
-    return determineOuput(build);
+
+    return determineOuput(build, firstLetter, lastLetter);
 }
 
-function determineOuput(build: Map<string, bigint>) {
-    const amountOfLettersFirst = new Map<string, bigint>();
-    const amountOfLettersLast = new Map<string, bigint>();
+function determineOuput(build: Map<string, bigint>, firstLetter: string, lastLetter: string) {
     const amountOfLetters = new Map<string, bigint>();
 
+    // Count each letter in the keys of build.
     build.forEach((value: bigint, key: string) => {
-        const letterValue1: bigint = amountOfLettersFirst.get(key[0]) || BigInt(0);
+        const letterValue1: bigint = amountOfLetters.get(key[0]) || BigInt(0);
         const newValue = letterValue1 + value;
-        amountOfLettersFirst.set(key[0], newValue);
+        amountOfLetters.set(key[0], newValue);
 
-        const letterValue2: bigint = amountOfLettersLast.get(key[1]) || BigInt(0);
+        const letterValue2: bigint = amountOfLetters.get(key[1]) || BigInt(0);
         const newValue2 = letterValue2 + value;
-        amountOfLettersLast.set(key[1], newValue2);
+        amountOfLetters.set(key[1], newValue2);
     });
 
-    amountOfLettersFirst.forEach((value: bigint, key: string) => {
-        const lastValue: bigint = amountOfLettersLast.get(key) || BigInt(0);
-        if (value > lastValue) {
-            amountOfLetters.set(key, value);
-        } else {
-            amountOfLetters.set(key, lastValue);
-        }
+    // Since almost each letter was just counted twice, halve the values!
+    amountOfLetters.forEach((value: bigint, key: string) => {
+        amountOfLetters.set(key, value / BigInt(2));
     });
+
+    // The first letter was only counted once, but still divided for, so add one.
+    let amountFirstLetter = amountOfLetters.get(firstLetter) || BigInt(0);
+    amountFirstLetter = amountFirstLetter + BigInt(1);
+    amountOfLetters.set(firstLetter, amountFirstLetter);
+    // Same for the last letter
+    let amountLastLetter = amountOfLetters.get(lastLetter) || BigInt(0);
+    amountLastLetter = amountLastLetter + BigInt(1);
+    amountOfLetters.set(lastLetter, amountLastLetter);
 
     let mostOccurringAmount = BigInt(0);
     let leastOccurringAmount = BigInt(-1);
-
     amountOfLetters.forEach((value: bigint) => {
         if (value > mostOccurringAmount) {
             mostOccurringAmount = value;
@@ -52,12 +58,6 @@ function determineOuput(build: Map<string, bigint>) {
         }
     });
 
-    // Part 1 code zegt dat het dit moe tzijn voor 10 loops:
-    // 3573
-    // 515;
-    // 3058;
-    console.log(mostOccurringAmount);
-    console.log(leastOccurringAmount);
     return mostOccurringAmount - leastOccurringAmount;
 }
 
@@ -69,11 +69,12 @@ function applyRules(build: Map<string, bigint>, rules: string[][]) {
         rules.forEach((rule: string[]) => {
             if (key === rule[0]) {
                 const nextKey1: string = rule[0][0] + rule[1];
-                const nextKey2: string = rule[1] + rule[0][1];
-                // Other rules may result in the same keys, so check for their value before setting it
                 const value1 = newBuild.get(nextKey1) || BigInt(0);
-                const value2 = newBuild.get(nextKey2) || BigInt(0);
                 newBuild.set(nextKey1, value1 + BigInt(oldValue));
+
+                // Other rules may result in the same keys, so check for their value before setting it
+                const nextKey2: string = rule[1] + rule[0][1];
+                const value2 = newBuild.get(nextKey2) || BigInt(0);
                 newBuild.set(nextKey2, value2 + BigInt(oldValue));
             }
         });
@@ -82,9 +83,11 @@ function applyRules(build: Map<string, bigint>, rules: string[][]) {
 }
 
 function fillInitialMap(buildString: string) {
-    const build = new Map();
+    const build = new Map<string, bigint>();
     for (let i = 1; i < buildString.length; i++) {
-        build.set(buildString[i - 1] + buildString[i], 1);
+        const key = buildString[i - 1] + buildString[i];
+        const prevValue = build.get(key) || BigInt(0);
+        build.set(key, prevValue + BigInt(1));
     }
     return build;
 }

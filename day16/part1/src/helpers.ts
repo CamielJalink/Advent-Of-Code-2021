490e01fcc857eec34c608bb0b9474e4c4440f258
Camiel <camiel.jalink@gmail.com>
2021-12-22 18:09:34 +0100

    Some day16 progress

---
+++ day16/part1/src/helpers.ts
@@ -0,0 +1,79 @@
+export function parseHexToBinary(input: string) {
+    let bitInput = "";
+    for (const char of input) {
+        switch (char) {
+            case "0":
+                bitInput += "0000";
+                break;
+            case "1":
+                bitInput += "0001";
+                break;
+            case "2":
+                bitInput += "0010";
+                break;
+            case "3":
+                bitInput += "0011";
+                break;
+            case "4":
+                bitInput += "0100";
+                break;
+            case "5":
+                bitInput += "0101";
+                break;
+            case "6":
+                bitInput += "0110";
+                break;
+            case "7":
+                bitInput += "0111";
+                break;
+            case "8":
+                bitInput += "1000";
+                break;
+            case "9":
+                bitInput += "1001";
+                break;
+            case "A":
+                bitInput += "1010";
+                break;
+            case "B":
+                bitInput += "1011";
+                break;
+            case "C":
+                bitInput += "1100";
+                break;
+            case "D":
+                bitInput += "1101";
+                break;
+            case "E":
+                bitInput += "1110";
+                break;
+            case "F":
+                bitInput += "1111";
+                break;
+            default:
+                break;
+        }
+    }
+
+    return bitInput;
+}
+
+export function removeTrailingZeroes(binary: string) {
+    let cleanedBinary = binary;
+    for (let i = binary.length - 1; i > 0; i--) {
+        if (binary[i] === "0") {
+            cleanedBinary = binary.substring(0, i);
+        } else {
+            break;
+        }
+    }
+    return cleanedBinary;
+}
+
+export function findLiteral(body: string) {
+    let binaryLiteralString = "";
+    for (let i = 5; i <= body.length; i += 5) {
+        binaryLiteralString += body.substring(i - 4, i);
+    }
+    return parseInt(binaryLiteralString, 2);
+}
--- day16/part1/src/index.ts
+++ day16/part1/src/index.ts
@@ -1,9 +1,26 @@
 import { readFileSync } from "fs";
+import { parseHexToBinary, removeTrailingZeroes, findLiteral } from "./helpers";
 
 function advent() {
     const stringInput = readFileSync("input.txt", "utf-8");
     const input = stringInput.split("\r\n");
-    console.log(input);
+    parseInput(input);
+}
+
+function parseInput(input: string[]) {
+    let packet = parseHexToBinary(input[0]);
+    packet = removeTrailingZeroes(packet);
+
+    // header = version and then typeId. The rest is the body.
+    const version = parseInt(packet.substring(0, 3), 2);
+    const typeId = parseInt(packet.substring(3, 6), 2);
+    const packetBody = packet.substring(6);
+
+    // typeId = 4 means: find the literal number hidden in the body.
+    if (typeId === 4) {
+        const literalValue = findLiteral(packetBody);
+        console.log(literalValue);
+    }
 }
 
 advent();

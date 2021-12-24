0ce61da5d0e3f9067360f7becb76f28baf7b15e4
Camiel <camiel.jalink@gmail.com>
2021-12-24 23:38:15 +0100

    copied part2 directory from part1

---
+++ day17/part2/src/index.ts
@@ -0,0 +1,72 @@
+import { readFileSync } from "fs";
+import { parseInput, determineVelocities } from "./helpers";
+
+function advent() {
+    const stringInput = readFileSync("input.txt", "utf-8");
+    const input = stringInput.split("\r\n");
+    console.log(determineHighestY(input[0]));
+}
+
+function determineHighestY(input: string) {
+    const dimensions: number[] = parseInput(input);
+    const relevantVelocities: number[][] = determineVelocities(dimensions);
+    let highestY = 0;
+    relevantVelocities.forEach((velocity: number[]) => {
+        const testShotHighestY = testShot(velocity, dimensions);
+        if (highestY < testShotHighestY) {
+            highestY = testShotHighestY;
+        }
+    });
+    return highestY;
+}
+
+function testShot(velocity: number[], dimensions: number[]) {
+    let velX = velocity[0];
+    let velY = velocity[1];
+    const minX = dimensions[0];
+    const maxX = dimensions[1];
+    const maxY = dimensions[2]; // lowest Y is max Y... right?
+    const minY = dimensions[3];
+
+    let x = 0;
+    let y = 0;
+    let highestY = 0;
+    let shotValid = false;
+    let stillChecking = true;
+
+    while (stillChecking) {
+        if (y > highestY) {
+            highestY = y;
+        }
+
+        // If the probe is in the target zone, this is a succesfull shot!
+        if (x >= minX && x <= maxX && y <= minY && y >= maxY) {
+            shotValid = true;
+            stillChecking = false;
+        }
+
+        // determine our next location based on current velocity.
+        x += velX;
+        y += velY;
+
+        // detect whether we've overshot the target.
+        if (x > maxX || y < maxY) {
+            stillChecking = false;
+        }
+
+        // Horizontal velocity is dampened by water pressure.
+        if (velX > 0) {
+            velX--;
+        }
+        // y-velocity always feels gravity.
+        velY--;
+    }
+
+    if (shotValid) {
+        return highestY;
+    } else {
+        return 0;
+    }
+}
+
+advent();
